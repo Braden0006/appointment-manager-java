a77ce871a316cb9eb7a4c75b94d685ddd31560e7
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2025-08-08 14:26:09 -0600

    Added some dependencies and made a static html page

--- src/main/java/org/example/taskmanager/TaskManagerApplication.java
+++ src/main/java/org/example/taskmanager/TaskManagerApplication.java
@@ -2,14 +2,12 @@ package org.example.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
+        System.out.println("Hello world!!");
     }
 }
