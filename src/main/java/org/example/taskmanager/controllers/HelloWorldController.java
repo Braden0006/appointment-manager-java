d3466e34ce300470c0e609cb6b885d8c9f40cb66
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2025-08-08 17:56:23 -0600

    Added content to the thymeleaf page with attributes in the controller

--- src/main/java/org/example/taskmanager/controllers/HelloWorldController.java
+++ src/main/java/org/example/taskmanager/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package org.example.taskmanager.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,8 +10,15 @@ public class HelloWorldController {
 
     @GetMapping("/test")
     @ResponseBody
-
     public String printHello() {
         return "Hello Worldies!!!";
+    }
+
+    @GetMapping("/test2")
+    public String printHello2(Model model) {
+        model.addAttribute("message", "Have a great day!");
+        model.addAttribute("rating", "99");
+        model.addAttribute("report", "really good!!");
+        return "helloPage";
     }
 }
