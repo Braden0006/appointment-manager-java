12272df9385bf26fc90f9d5306af758f04bae7b7
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2025-08-08 18:51:51 -0600

    Learned how to use the 'ModelAndView' library to programmatically add content to an existing page

--- src/main/java/org/example/taskmanager/controllers/HelloWorldController.java
+++ src/main/java/org/example/taskmanager/controllers/HelloWorldController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorldController {
@@ -20,5 +21,16 @@ public class HelloWorldController {
         model.addAttribute("rating", "99");
         model.addAttribute("report", "really good!!");
         return "helloPage";
+    }
+
+    @GetMapping("/test3")
+    public ModelAndView displayHello() {
+        ModelAndView mv = new ModelAndView();
+
+        mv.addObject("message", "Have a super day!");
+        mv.addObject("rating", "55");
+        mv.setViewName("helloPage");
+
+        return mv;
     }
 }
