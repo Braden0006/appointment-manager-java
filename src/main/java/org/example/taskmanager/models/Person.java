a02895aa102344bb4ae9a9137d50c8797c780394
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2025-08-11 21:23:44 -0600

    Created 'models' package and 'Person' class

---
+++ src/main/java/org/example/taskmanager/models/Person.java
@@ -0,0 +1,57 @@
+package org.example.taskmanager.models;
+
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+    private String birth;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", birth='" + birth + '\'' +
+                '}';
+    }
+
+    public Person(int id, String birth, int age, String name) {
+        this.id = id;
+        this.birth = birth;
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+}
