a0830c43f056540559a071c255cd165349e00b05
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2025-08-11 20:50:21 -0600

    Used the '@RequestParam' annotation to display web address parameters on the screen

--- src/main/java/org/example/taskmanager/controllers/HelloWorldController.java
+++ src/main/java/org/example/taskmanager/controllers/HelloWorldController.java
@@ -3,6 +3,7 @@ package org.example.taskmanager.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,7 +17,7 @@ public class HelloWorldController {
     }
 
     @GetMapping("/test2")
-    public String printHello2(Model model) {
+    public String showHello2(Model model) {
         model.addAttribute("message", "Have a great day!");
         model.addAttribute("rating", "99");
         model.addAttribute("report", "really good!!");
@@ -32,5 +33,14 @@ public class HelloWorldController {
         mv.setViewName("helloPage");
 
         return mv;
+    }
+
+    @GetMapping("/test4")
+    public String showHello4(@RequestParam("message") String message, Model model) {
+        //model.addAttribute("message", "Have a great day!");
+        model.addAttribute("message", message);
+        model.addAttribute("rating", "99");
+        model.addAttribute("report", "really good!!");
+        return "helloPage";
     }
 }
