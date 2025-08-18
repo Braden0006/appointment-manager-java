29754a62f07ac2e14a9c47846f3dedbd5221ecd8
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2025-08-18 13:33:40 -0600

    Successfully created 'homePage.html' with thymeleaf to display tasks

--- src/main/java/org/example/taskmanager/TaskManagerApplication.java
+++ src/main/java/org/example/taskmanager/TaskManagerApplication.java
@@ -3,12 +3,22 @@ package org.example.taskmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @SpringBootApplication
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
-        System.out.println("Hello world!!");
+    }
+
+    @GetMapping("/")
+
+    public String displayTasks() {
+        return "homePage";
     }
 }
