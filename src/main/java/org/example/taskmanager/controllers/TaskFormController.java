0d75a7fb7ed895392218fe9193a0af0712f5344e
Braden0006 <bradenwhitch@gmail.com>
2025-08-20 11:59:17 -0600

    Implemented 'Model' object into task form controller

--- src/main/java/org/example/taskmanager/controllers/TaskFormController.java
+++ src/main/java/org/example/taskmanager/controllers/TaskFormController.java
@@ -1,6 +1,8 @@
 package org.example.taskmanager.controllers;
 
+import org.example.taskmanager.handlingTaskSubmission.Task;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,8 +10,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 class TaskFormController {
 
-    @GetMapping("/task")
-    public String taskForm() {
+    @GetMapping("/appointment")
+    public String taskForm(Model model) {
+        model.addAttribute("createTaskPage", new Task());
         return "createTaskPage";
     }
 }
