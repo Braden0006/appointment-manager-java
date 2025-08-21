fb35ec0529bc93cb0f4d2c810dd9ac7f5fd4a80c
Braden0006 <bradenwhitch@gmail.com>
2025-08-20 18:52:02 -0600

    Added '@PostMapping' annotation with 'taskSubmit' function to task from controller

--- src/main/java/org/example/taskmanager/controllers/TaskFormController.java
+++ src/main/java/org/example/taskmanager/controllers/TaskFormController.java
@@ -3,9 +3,7 @@ package org.example.taskmanager.controllers;
 import org.example.taskmanager.handlingTaskSubmission.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 class TaskFormController {
@@ -14,5 +12,11 @@ class TaskFormController {
     public String taskForm(Model model) {
         model.addAttribute("appointment", new Task());
         return "createTaskPage";
+    }
+
+    @PostMapping("/appointment")
+    public String taskSubmit(@ModelAttribute Task appointment, Model model) {
+       model.addAttribute("appointment", appointment);
+       return "/homePage";
     }
 }
